5b21c06ac84efb02d5bb90780058d9ec4139e2ab
keshaka <88092546+keshaka@users.noreply.github.com>
2022-12-28 10:18:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = "MongoDB"
 COLLECTION_NAME = 'Telegram_files'
 
 # Others
-LOG_CHANNEL = int(-1001799057674')
+LOG_CHANNEL = int(-1001799057674)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'search_zone_support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
